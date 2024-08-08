64ca6e24aa1ed114230d0d7c945634c655e267c1
Vishnu tyagi <vishnu.tyagi.mec20@itbhu.ac.in>
2024-08-08 13:21:04 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,13 @@ import React from 'react'
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+        <ol key={relativeList}>
+          <li key={relativeListItem1}> item 1</li>
+          <li key={relativeListItem2}> item 2</li>
+          <li key={relativeListItem3}> item 3</li>
+          <li key={relativeListItem4}> item 4</li>
+          <li key={relativeListItem5}> item 5</li>
+        </ol>
     </div>
   )
 }
