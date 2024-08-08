4659ae6c9594be953530a3b57ce3f66778a8cf82
Vishnu tyagi <vishnu.tyagi.mec20@itbhu.ac.in>
2024-08-08 13:25:07 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,14 @@ import React from 'react'
 const App = () => {
   return (
     <div id="main">
-        <ol key={relativeList}>
-          <li key={relativeListItem1}> item 1</li>
-          <li key={relativeListItem2}> item 2</li>
-          <li key={relativeListItem3}> item 3</li>
-          <li key={relativeListItem4}> item 4</li>
-          <li key={relativeListItem5}> item 5</li>
+        <ol key={"relativeList"}>
+          <li key={"relativeListItem1"}> item 1</li>
+          <li key={"relativeListItem2"}> item 2</li>
+          <li key={"relativeListItem3"}> item 3</li>
+          <li key={"relativeListItem4"}> item 4</li>
+          <li key={"relativeListItem5"}> item 5</li>
         </ol>
+        cool cool
     </div>
   )
 }
